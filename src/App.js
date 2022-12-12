b4be237ab6fdf5ee7a44b377670630a4fd524420
daniyal <daniyalmuhammad977@gmail.com>
2022-12-12 19:55:10 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+let name = "daniyal ullah khan"
+function App() {
+  return (
+    <>
+      {/* <nav>
+      <li>home</li>
+      <li>about</li>
+      <li>contact</li>
+    </nav>
+    <h1>hi here is {name}</h1>
+    <div classNameName="containe">
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione quos eos minima atque alias reprehenderit neque iste sint eius iusto, autem animi possimus.</p>
+    </div>
+    <div classNameName="secc">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia cum voluptas totam minima cupiditate. Labore debitis veritatis rem facere, hic eius amet nihil.
+    </div> */}
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
